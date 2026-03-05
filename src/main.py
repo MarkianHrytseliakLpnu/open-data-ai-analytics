199bc886efb6d979afe1cb4a02174f140f269694
Markian Hrytseliak <markiian.hrytseliak.shi.2023@lpnu.ua>
2026-03-05 13:38:46 +0200

    Changes: - research.py: added method that performs data research analysis - main.py: added research.py run script

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from src import config
 from src import loader
+from src import research  # <--- Додаємо імпорт research
 
 
 def main():
@@ -9,19 +10,28 @@ def main():
     print("--- Open Data AI Analytics Pipeline ---")
 
     # Етап 1: Завантаження даних
-    print("[Step 1] Data Loading...")
+    print("\n>>> [Step 1] Data Loading...")
     try:
-        loader.download_data(config.DATASET_URL, config.RAW_DATA_FILE_PATH)
+        if not config.RAW_DATA_FILE_PATH.exists():
+            loader.download_data(config.DATASET_URL, config.RAW_DATA_FILE_PATH)
+        else:
+            print("Data file already exists. Skipping download.")
     except Exception as e:
         print(f"Critical error at Data Loading stage: {e}")
-        return  # Зупиняємо виконання, якщо завантаження не вдалось
+        return
+
+    # Етап 3: Дослідження даних (Research)
+    print("\n>>> [Step 3] Data Research...")
+    try:
+        research.analyze_research(config.RAW_DATA_FILE_PATH)
+    except Exception as e:
+        print(f"Error at Data Research stage: {e}")
 
     # Тут будуть наступні етапи:
-    # print("[Step 2] Data Quality Analysis...")
-    # quality.check(...)
+    # print("\n>>> [Step 4] Visualization...")
+    # visualization.create_visuals(...)
 
-    print("--- Pipeline finished successfully ---")
-
+    print("\n--- Pipeline finished successfully ---")
 
 if __name__ == "__main__":
     main()
